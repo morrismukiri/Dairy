fe9166fdaca16abe178ff9ab774b4e4fbfc9f43b
Morris Mukiri <morrismukiri@gmail.com>
2015-12-05 22:07:58 +0300

    added new Database

--- dairy.sql
+++ dairy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.5.8.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 02, 2013 at 09:29 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.4.15
+-- Generation Time: Dec 05, 2015 at 10:05 PM
+-- Server version: 5.5.35
+-- PHP Version: 5.4.23
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `delivery` (
   PRIMARY KEY (`id`),
   KEY `r_f_no` (`r_f_no`),
   KEY `r_received_by` (`r_received_by`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
 -- Dumping data for table `delivery`
@@ -62,7 +62,8 @@ INSERT INTO `delivery` (`id`, `r_f_no`, `r_kg`, `r_dt`, `r_received_by`, `r_deli
 (23, '11', 41, '2013-05-04 17:37:52', '', ''),
 (24, '11', 40, '2013-05-26 16:08:04', '', ''),
 (25, '11', 30, '2013-05-07 16:09:41', '', ''),
-(26, '86', 45, '2013-05-26 17:06:57', '', 'rean');
+(26, '86', 45, '2013-05-26 17:06:57', '', 'rean'),
+(27, '9001', 10, '2015-11-26 15:40:41', '', 'Kathambi');
 
 -- --------------------------------------------------------
 
@@ -82,16 +83,18 @@ CREATE TABLE IF NOT EXISTS `employees` (
   UNIQUE KEY `e_name` (`e_name`),
   UNIQUE KEY `e_payroll_no_2` (`e_payroll_no`),
   KEY `e_payroll_no` (`e_payroll_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `employees`
 --
 
 INSERT INTO `employees` (`id`, `e_name`, `e_mail`, `username`, `e_pass`, `e_role`, `e_payroll_no`) VALUES
-(2, 'kelly', 'kelly@ciangoi.co.ke', '', '1234', 'Supervisor', 'e1'),
-(3, 'Catherine Muendi', 'cnmuend@yahoo.com', '', '12345', 'Manager', '3456'),
-(4, 'Janet Mwende', 'jane@ciangoi.co.ke', '', '1234', 'Clerk', 'e2');
+(2, 'kelly', 'kelly@ciangoi.co.ke', '', '827ccb0eea8a706c4c34a16891f84e7b', 'Supervisor', 'e1'),
+(3, 'Catherine Muendi', 'cnmuend@yahoo.com', '', '827ccb0eea8a706c4c34a16891f84e7b', 'Manager', '3456'),
+(4, 'Janet Mwende', 'jane@ciangoi.co.ke', '', 'd41d8cd98f00b204e9800998ecf8427e', 'Clerk', 'e2'),
+(5, 'Mike Kithinji', 'kithinjinthiiri@gmail.com', '', '482c811da5d5b4bc6d497ffa98491e38', 'Manager', '90909'),
+(6, 'John Muthure', 'john@ciangoi.co.ke', '', '827ccb0eea8a706c4c34a16891f84e7b', 'Clerk', '343');
 
 -- --------------------------------------------------------
 
@@ -129,6 +132,7 @@ INSERT INTO `farmers` (`f_no`, `f_id`, `f_name`, `f_locallity`, `f_ac`, `last_pa
 ('8', '99', 'joy kanampiu', 'kibumbu', '879273', '2013-04-30', '09278', ''),
 ('86', '34257', 'Timothy ndegwa', 'mungoni centre', '7178110987', '2013-05-26', '67895', NULL),
 ('900', '454', 'Leonard Mabo', '12', '346743', '2013-04-30', '22', ''),
+('9001', '22908070', 'Martin Ireri', 'Njaina', '459141241', NULL, '+254722339900', NULL),
 ('901', '451', 'Jane Kobi', 'Thika', '34624', '2013-04-30', '78342', '');
 
 -- --------------------------------------------------------
