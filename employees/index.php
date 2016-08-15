4f76214a7b4abf143c27f2d9bedbdf3988b70d86
Morris Mukiri <morrismukiri@gmail.com>
2016-08-15 15:32:31 +0300

    removed unnecessary files

--- employees/index.php
+++ employees/index.php
@@ -6,16 +6,17 @@ if ($current_user['role'] != 'Manager') {
 echo "sorry you are not allowed to access this module";
 exit();
 }
- echo '<a class="btn btn-large btn-primary" href="add.php"><i class="icon-plus icon-white"></i>New Employee</a><br/><br/>';
-echo "<table class='table table-hover table-striped table-condensed table-bordered'>"; 
-    echo "<thead><tr>"; 
-        //echo "<th>Id</th>"; 
-        echo "<th> Name</th>"; 
-        echo "<th> Mail</th>"; 
-//        echo "<th> Pass</th>"; 
-        echo "<th> Role</th>"; 
-        echo "<th> Payroll No</th>"; 
-        echo "<th>Actions</th></tr> </thead><tbody>"; 
+?><a class="btn btn-large btn-primary" href="add.php"><i class="icon-plus icon-white"></i>New Employee</a><br/><br/>
+<table class='table table-hover table-striped table-condensed table-bordered'>
+    <thead><tr>
+        <!-- //<th>Id</th>";  -->
+        <th> Name</th>
+        <th> Mail</th>
+<!-- //        <th> Pass</th>";  -->
+        <th> Role</th>
+        <th> Payroll No</th>
+        <th>Actions</th></tr> </thead><tbody>
+        <?php
     $result = mysql_query("SELECT * FROM `employees`") or trigger_error(mysql_error()); 
     while($row = mysql_fetch_array($result)){ 
     foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
