6d537053c4a5493b8874fabb25945836d86d5422
Morris Mukiri <morrismukiri@gmail.com>
2016-09-13 21:17:37 +0300

    demo users

--- dairy.sql
+++ dairy.sql
@@ -1,20 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.8.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 05, 2015 at 10:05 PM
--- Server version: 5.5.35
--- PHP Version: 5.4.23
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2016 at 08:16 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `dairy`
@@ -26,17 +26,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `delivery`
 --
 
-CREATE TABLE IF NOT EXISTS `delivery` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `delivery` (
+  `id` int(11) NOT NULL,
   `r_f_no` varchar(50) NOT NULL,
   `r_kg` float NOT NULL,
   `r_dt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `r_received_by` varchar(50) NOT NULL,
-  `r_deliverer` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `r_f_no` (`r_f_no`),
-  KEY `r_received_by` (`r_received_by`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+  `r_deliverer` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `delivery`
@@ -71,30 +68,25 @@ INSERT INTO `delivery` (`id`, `r_f_no`, `r_kg`, `r_dt`, `r_received_by`, `r_deli
 -- Table structure for table `employees`
 --
 
-CREATE TABLE IF NOT EXISTS `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
   `e_name` varchar(50) NOT NULL,
   `e_mail` varchar(50) DEFAULT NULL,
   `username` varchar(50) NOT NULL,
   `e_pass` varchar(50) NOT NULL,
   `e_role` varchar(50) DEFAULT NULL,
-  `e_payroll_no` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `e_name` (`e_name`),
-  UNIQUE KEY `e_payroll_no_2` (`e_payroll_no`),
-  KEY `e_payroll_no` (`e_payroll_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `e_payroll_no` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employees`
 --
 
 INSERT INTO `employees` (`id`, `e_name`, `e_mail`, `username`, `e_pass`, `e_role`, `e_payroll_no`) VALUES
-(2, 'kelly', 'kelly@ciangoi.co.ke', '', '827ccb0eea8a706c4c34a16891f84e7b', 'Supervisor', 'e1'),
 (3, 'Catherine Muendi', 'cnmuend@yahoo.com', '', '827ccb0eea8a706c4c34a16891f84e7b', 'Manager', '3456'),
-(4, 'Janet Mwende', 'jane@ciangoi.co.ke', '', 'd41d8cd98f00b204e9800998ecf8427e', 'Clerk', 'e2'),
-(5, 'Mike Kithinji', 'kithinjinthiiri@gmail.com', '', '482c811da5d5b4bc6d497ffa98491e38', 'Manager', '90909'),
-(6, 'John Muthure', 'john@ciangoi.co.ke', '', '827ccb0eea8a706c4c34a16891f84e7b', 'Clerk', '343');
+(7, 'Awesome Supervisor', 'supervisor@example.com', '', '1425d5d3160aa6bd140605cc75e63ce0', 'Supervisor', '6'),
+(8, 'Clerk Kent', 'clerk@example.com', '', 'ad4ac7fa40b0af2bae7374c57173f26c', 'Clerk', '7'),
+(9, 'Overall Manager', 'manager@example.com', '', '0795151defba7a4b5dfa89170de46277', 'Manager', '1');
 
 -- --------------------------------------------------------
 
@@ -102,7 +94,7 @@ INSERT INTO `employees` (`id`, `e_name`, `e_mail`, `username`, `e_pass`, `e_role
 -- Table structure for table `farmers`
 --
 
-CREATE TABLE IF NOT EXISTS `farmers` (
+CREATE TABLE `farmers` (
   `f_no` varchar(50) NOT NULL,
   `f_id` text NOT NULL,
   `f_name` varchar(50) NOT NULL,
@@ -110,9 +102,7 @@ CREATE TABLE IF NOT EXISTS `farmers` (
   `f_ac` varchar(50) DEFAULT NULL,
   `last_paid` date DEFAULT NULL,
   `f_phone` varchar(20) DEFAULT NULL,
-  `f_photo` blob,
-  PRIMARY KEY (`f_no`),
-  KEY `f_no` (`f_no`)
+  `f_photo` blob
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -123,6 +113,7 @@ INSERT INTO `farmers` (`f_no`, `f_id`, `f_name`, `f_locallity`, `f_ac`, `last_pa
 ('1', '23456779', 'alexandar jones', 'kk', '9890485987', '2013-04-30', '0721274242', NULL),
 ('11', '246890', 'Henry Muthee', 'Kibumbu', '3456423', '2013-04-30', '0987654', NULL),
 ('23', '234567', 'mokua Joe', 'kathigiririni', '3456423', '2013-04-30', '2343562', NULL),
+('234', '22552355', 'John Mayer', 'Area B', '12412421', NULL, '+2547224455', NULL),
 ('452', '345678', 'Jane nduta', 'kathigiririni', '5689', '2013-04-30', '4579', NULL),
 ('456785', '4456754457', 'DANIEL', 'KE', '4213', '2013-05-26', '2467', ''),
 ('49', '4456754', 'Michael Muasya musyimi', 'mungoni centre', '9890485', '2013-04-30', '2023586', ''),
@@ -141,17 +132,14 @@ INSERT INTO `farmers` (`f_no`, `f_id`, `f_name`, `f_locallity`, `f_ac`, `last_pa
 -- Table structure for table `payment`
 --
 
-CREATE TABLE IF NOT EXISTS `payment` (
+CREATE TABLE `payment` (
   `id` int(11) NOT NULL,
   `p_to` varchar(50) NOT NULL,
   `p_date` date NOT NULL,
   `p_ac` bigint(20) NOT NULL,
   `p_method` varchar(30) NOT NULL,
   `p_transaction_code` int(11) NOT NULL COMMENT 'Bank or Mpesa confirmation or receipt no',
-  `p_transacted_by` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `p_to` (`p_to`),
-  KEY `p_transacted_by` (`p_transacted_by`)
+  `p_transacted_by` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -160,13 +148,12 @@ CREATE TABLE IF NOT EXISTS `payment` (
 -- Table structure for table `settings_rates`
 --
 
-CREATE TABLE IF NOT EXISTS `settings_rates` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `settings_rates` (
+  `id` int(11) NOT NULL,
   `from` date NOT NULL,
   `to` date NOT NULL,
-  `rate` float NOT NULL COMMENT 'sh per kg',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `rate` float NOT NULL COMMENT 'sh per kg'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `settings_rates`
@@ -176,6 +163,67 @@ INSERT INTO `settings_rates` (`id`, `from`, `to`, `rate`) VALUES
 (4, '2013-01-01', '2013-01-31', 20),
 (5, '2013-03-01', '2013-03-31', 24);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `r_f_no` (`r_f_no`),
+  ADD KEY `r_received_by` (`r_received_by`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `e_name` (`e_name`),
+  ADD UNIQUE KEY `e_payroll_no_2` (`e_payroll_no`),
+  ADD KEY `e_payroll_no` (`e_payroll_no`);
+
+--
+-- Indexes for table `farmers`
+--
+ALTER TABLE `farmers`
+  ADD PRIMARY KEY (`f_no`),
+  ADD KEY `f_no` (`f_no`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `p_to` (`p_to`),
+  ADD KEY `p_transacted_by` (`p_transacted_by`);
+
+--
+-- Indexes for table `settings_rates`
+--
+ALTER TABLE `settings_rates`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `delivery`
+--
+ALTER TABLE `delivery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `settings_rates`
+--
+ALTER TABLE `settings_rates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
